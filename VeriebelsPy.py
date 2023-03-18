d295a8269f3ff92909b301d61ac35e91892c35da
Keernex <maksim1983vin@gmail.com>
2023-03-18 20:38:20 +0200

    Ex-1 done

--- VeriebelsPy.py
+++ VeriebelsPy.py
@@ -1,1 +1,8 @@
-
+# -*- coding: cp1251 -*-
+#Завдання 1
+print("Task 1")
+q = int(input(' namber 1: '))
+w = int(input(' namber 2: '))
+e = int(input(' namber 3: '))
+print(f'{q}+{w}+{e}={q+w+e}')
+print(f'{q}*{w}*{e}={q*w*e}')
